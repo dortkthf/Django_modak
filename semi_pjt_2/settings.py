c87bde5187f4fee2fdedeea990a5ee52bf12daea
dortkthf <dortkthf@gmail.com>
2022-11-21 22:45:12 +0900

    fix

--- semi_pjt_2/settings.py
+++ semi_pjt_2/settings.py
@@ -29,12 +29,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "Ture"
 
 ALLOWED_HOSTS = [
-    "Modakelastic-env.eba-czwvtwgp.ap-northeast-2.elasticbeanstalk.com",  # 예시입니다. 본인 URL로 해주세요.
     "127.0.0.1",
     "localhost",
+    "Kdt6team-env.eba-rmvmzut2.ap-northeast-2.elasticbeanstalk.com",
 ]
 
 
@@ -44,14 +44,13 @@ INSTALLED_APPS = [
     "mathfilters",
     "notes",
     "shoppings",
-    "chat",
     "cards",
     "vocies",
     "accounts",
     "meetings",
     "widget_tweaks",
-    "storages",
     "imagekit",
+    "storages",
     "django_bootstrap5",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -93,6 +92,28 @@ TEMPLATES = [
 WSGI_APPLICATION = "semi_pjt_2.wsgi.application"
 
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "kdt_6_rds", # 코드 블럭 아래 이미지 참고하여 입력
+#         "USER": "postgres",
+#         "PASSWORD": "faker1121", # 데이터베이스 생성 시 작성한 패스워드
+#         "HOST": "kdt-6-rds.cjszo6864ame.ap-northeast-2.rds.amazonaws.com", # 코드 블럭 아래 이미지 참고하여 입력
+#         "PORT": "5432",
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -130,7 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
 STATIC_ROOT = "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -147,7 +170,19 @@ if DEBUG:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+
 else:
+    # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "semi_pjt_2.storages.MediaStorage"
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+
+    AWS_REGION = "ap-northeast-2"
+    AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (
+        AWS_STORAGE_BUCKET_NAME,
+        AWS_REGION,
+    )
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
@@ -158,18 +193,7 @@ else:
             "PORT": "5432",
         }
     }
-    DEFAULT_FILE_STORAGE = "semi_pjt_2.storages.MediaStorage"
 
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-
-    AWS_REGION = "ap-northeast-2"
-    AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (
-        AWS_STORAGE_BUCKET_NAME,
-        AWS_REGION,
-    )
-    STATIC_ROOT = BASE_DIR / "static"
 
 AUTH_USER_MODEL = "accounts.User"
 
@@ -187,6 +211,7 @@ MESSAGE_TAGS = {
 """
 기존 DATABASES 코드 아래에 아래 세 줄을 추가합니다.
 """
+
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
@@ -201,5 +226,3 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
-# AUTH_USER_MODEL = "accounts.user"
